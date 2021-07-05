a948c54517686ba7e09e4c23565ddaf20f5dbf53
Mateusz GABRYŚ <mateuszigabrys@gmail.com>
2021-07-05 11:58:07 +0200

    Seed MongoDB with crimeevents

---
+++ ReportCrime/Crime.API/Controllers/CrimeController.cs
@@ -0,0 +1,31 @@
+﻿using Crime.API.Data;
+using Crime.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crime.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CrimeController : Controller
+    {
+        private readonly ILogger<CrimeController> _logger;
+        private readonly CrimeContext _context;
+        public CrimeController(ILogger<CrimeController> logger, CrimeContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<CrimeEvent>> Get()
+        {
+            return await _context.Crimes.Find(p => true).ToListAsync();
+        }
+    }
+}
---
+++ ReportCrime/Crime.API/Models/CrimeEvent.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crime.API.Models
+{
+    public class CrimeEvent
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CrimeId { get; set; }
+        public TypeOfEvent TypeOfEvent { get; set; } = TypeOfEvent.Assault;
+        public string Description { get; set; }
+        public string PlaceOfEvent { get; set; }
+        public string ReportingPersonEmail { get; set; }
+        public StatusEvent Status { get; set; } = StatusEvent.Waiting;
+        public string LawEnforcementId { get; set; }
+    }
+}
